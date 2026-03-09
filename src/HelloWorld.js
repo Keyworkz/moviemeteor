b8a4743ebeac84219b87abd1d76df6a6f9c7e94c
Keyworkz <info@keyworkz.nl>
2026-03-09 22:08:13 +0100

    creation helloworld function

---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+function HelloWorld() {
+  return (
+    <Fragment>
+      <hr />
+      <h1>Hello, Ryan</h1>
+    </Fragment>
+  );
+}
+
+export default HelloWorld;
